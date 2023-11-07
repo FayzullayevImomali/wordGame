41d08495a2e679722b972a792582725b4526d2a8
Fayzullayev <fayzullayevimomali@mail.ru>
2023-11-07 18:34:40 +0500

    massage-!!

--- app/src/main/java/com/fayzullayev/word_game_app/SettingsActivity.kt
+++ app/src/main/java/com/fayzullayev/word_game_app/SettingsActivity.kt
@@ -1,11 +1,24 @@
 package com.fayzullayev.word_game_app
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowId
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import java.util.Objects
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var spinner: Spinner;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        spinner = findViewById(R.id.spinner);
+        val listItem = listOf("Ocean", "GreenNature", "Mountains")
+
     }
+
 }
